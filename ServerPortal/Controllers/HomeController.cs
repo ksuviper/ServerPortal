dce1bc87cfa66d9a5c7ada961efd55215fcb2b13
Adam Staggenborg <ksuviper@gmail.com>
2012-02-12 05:42:14 -0600

    Initial Build

---
+++ ServerPortal/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ServerPortal.Models;
+
+namespace ServerPortal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome the the Server Portal";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+        private static Media _DirList = new Media();
+        public ActionResult Media(string DirPath = "c:\\")
+        {
+            ViewBag.Message = "File Listing for: " + DirPath;
+
+            _DirList.getDirList(DirPath);
+            return View(_DirList._MediaList);
+        }
+    }
+}
---
+++ ServerPortal/Models/Media.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace ServerPortal.Models
+{
+    public class MediaInfo
+    {
+        public String FileName { get; set; }
+        public String Path { get; set; }
+        public Boolean isFolder { get; set; }
+        public String Size { get; set; }
+    }
+
+    public class Media
+    {
+        public List<MediaInfo> _MediaList = new List<MediaInfo>();
+        public void getDirList(string CurPath)
+        {            
+            _MediaList.Clear();
+
+            string[] Dirs = Directory.GetDirectories(CurPath);
+            foreach (string Item in Dirs)
+            {
+                _MediaList.Add(new MediaInfo
+                {
+                    FileName = new DirectoryInfo(Item).Name,
+                    isFolder = true,
+                    Path = Path.GetFullPath(Item),
+                    Size = ""
+                });
+                
+            }
+            string[] Files = Directory.GetFiles(CurPath);
+            foreach (string Item in Files)
+            {
+                _MediaList.Add(new MediaInfo
+                {
+                    FileName = Path.GetFileName(Item),
+                    isFolder = false,
+                    Path = Path.GetFullPath(Item),
+                    Size = new FileInfo(Item).Length.ToString()
+                });
+            }
+               
+        }
+    }
+}
