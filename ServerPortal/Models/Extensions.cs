9b058b39b7ac52e57cc3bc91aafb14dd843839a1
Andy Edinborough <andy@edinborough.org>
2012-02-13 10:07:10 -0600

    Removed static references to prevent issues with simultaneous requests; combiled models with their respective helper classes; added extension method for formatting file size

---
+++ ServerPortal/Models/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ServerPortal.Models {
+  public static class Extensions {
+
+    static readonly string[] _FormatSize = { "B", "KB", "MB", "GB", "TB", "XB" };
+    public static string FormatSize(this long sz) {
+      double nsz = sz;
+      for (int i = 0; i <= _FormatSize.Length - 1; i++) {
+        nsz /= i == 0 ? 1 : i > 1 ? 1000 : 1024;
+        if (nsz < 1024 || i == _FormatSize.Length - 1) {
+          return Math.Round(nsz, 1) + " " + _FormatSize[i];
+        }
+      }
+      return "0 B";
+    }
+
+  }
+}
